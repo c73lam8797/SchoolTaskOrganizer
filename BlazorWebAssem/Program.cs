ed6c8288315dc4c8d554e4ee08b5feac1ee7ac8c
Charmaine <c73lam@gmail.com>
2020-01-20 02:25:00 -0500

    initial commit

---
+++ BlazorWebAssem/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using static BlazorWebAssem.Pages.Deliverables;
+
+namespace BlazorWebAssem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
